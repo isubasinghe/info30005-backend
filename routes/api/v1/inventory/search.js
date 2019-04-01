8cbe003f7d4ed0aa9cc70a847fee9e42521f16c9
Abhisha Nirmalathas <nirmalathasa@student.unimelb.edu.au>
2019-04-01 22:08:45 +1100

    adding users to algolia index

--- routes/api/v1/inventory/search.js
+++ routes/api/v1/inventory/search.js
@@ -1,8 +1,23 @@
+const algoliasearch = require('algoliasearch');
+const client = algoliasearch(process.env.ALGOLIA_API_KEY, process.env.ALGOLIA_APP_ID);
 
+
+const index = client.initIndex('search');
 
 function search(req, res) {
-    req.app.locals.db.users;
-}
+    index.addObjects(request.app.locals.db.users, function(err, content){
+        if (err){
+            console.error(err);
+        }
+    });
+    index.setSettings({
+        // Select the attributes you want to search in
+        searchableAttributes: [
+          'items', 'location'
+        ]}, function(err, content){
+            console.log(content);
+      });
+};
 
 
 
