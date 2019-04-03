74e76cb4c44922c09c8215f43216f5e6775522a7
Abhisha Nirmalathas <nirmalathasa@student.unimelb.edu.au>
2019-04-03 13:53:08 +1100

    searches based on item name or location or both

--- routes/api/v1/inventory/search.js
+++ routes/api/v1/inventory/search.js
@@ -3,9 +3,16 @@ mongoose.set('useCreateIndex', true);
 var User = mongoose.model('Users');
 
 var search = function(req, res) {
-    var searchParameter = req.body.itemName;
-    User.find({items: {name: req.body.itemName}}, function(err, users){
+    var queryConditions = {}
+    if(req.body.itemName){
+        queryConditions.name = req.body.itemName;
+    }
+    if(req.body.location){
+        queryConditions.location = req.body.location;
+    }
+    User.find({items: queryConditions}, function(err, users){
         if(!err){
+            console.log(users)
             res.send(users);
         }else{
             res.sendStatus(404);
