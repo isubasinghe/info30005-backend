0f40ac22ca7fb7d6afe0c6896b1a08d4042a7207
Isitha Subasinghe <isubasinghe@student.unimelb.edu.au>
2019-04-05 08:06:56 +1100

    Added sign in, based upon JWT token auth

--- app.js
+++ app.js
@@ -6,20 +6,20 @@ const db = require('./models/db');
 
 
 const express = require('express');
-const session = require('./models/sessions');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const winston = require('winston');
 
 const routes = require('./routes');
+const jwt = require('./models/jwt');
 
 
 var app = express();
 app.locals.db = db;
 app.locals.winston = winston;
+app.locals.jwt = jwt;
 
-app.use(session);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
