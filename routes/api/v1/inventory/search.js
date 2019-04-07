927153c15b9a03d5b6cb4645cfe0759bdeddc286
Abhisha Nirmalathas <a.nirmalathas1@student.unimelb.edu.au>
2019-04-07 14:16:40 +1000

    tidied search code

--- routes/api/v1/inventory/search.js
+++ routes/api/v1/inventory/search.js
@@ -7,31 +7,19 @@ const User = mongoose.model('Users');
 
 let search = function(req, res) {
     let queryConditions = {}
-    if(req.body.name){
-        queryConditions.name = req.body.name;
-        User.find({items: {$elemMatch: queryConditions}}, function(err, user){
-            if(!err){
-                res.send(user);
-            }else{
-                res.sendStatus(404);
-            }
-        }).limit(10);
-    }
     if(req.body.location){
-        // Can use this for equality 
-        // User.find({items: {$elemMatch: {location: req.body.location}}}, function(err, user){
-        User.find({"items.location.coordinates": {$near: {$geometry: req.body.location}}}, 'items', function(err, user){
-            if(err){
-                console.log(err);
-                res.sendStatus(404);
-            }
-            else{
-                console.log("searchign");
-                res.send(user);
-            }
-        });
-        
+        queryConditions= {"items.location.coordinates": {$near: {$geometry: req.body.location}}};
     }
+    if(req.body.name){
+        queryConditions.items = {$elemMatch: {name: req.body.name}};
+    }
+    User.find(queryConditions,'items', function(err, item){
+        if(!err){
+            res.send(item);
+        }else{
+            res.sendStatus(404);
+        }
+    }).limit(10);
 };
 
 
