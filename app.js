fd64de0e1a748aad6d93def1310f2e0a443e6d41
Isitha Subasinghe <isubasinghe@student.unimelb.edu.au>
2019-04-25 18:08:21 +1000

    Enabled CORS

--- app.js
+++ app.js
@@ -20,6 +20,12 @@ app.locals.db = db;
 app.locals.winston = winston;
 app.locals.jwt = jwt;
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
